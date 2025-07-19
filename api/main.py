3de336e00734daf636c40ac59866eeb4d47a0430
rlagur9286 <rlagur9286@naver.com>
2025-07-19 21:28:13 +0900

    Move main.py to api/ directory for Vercel deployment

--- api/main.py
+++ api/main.py
@@ -1,5 +1,8 @@
-from app import app
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
