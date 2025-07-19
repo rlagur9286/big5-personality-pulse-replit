e7572f9cdc4f46565460f712c1664a867005225b
rlagur9286 <rlagur9286@naver.com>
2025-07-19 21:43:16 +0900

    Override homepage with simple HTML to bypass template rendering

--- api/main.py
+++ api/main.py
@@ -16,6 +16,16 @@ try:
             'template_folder': app.template_folder,
             'static_folder': app.static_folder
         }
+    
+    # 홈페이지를 간단한 텍스트로 오버라이드 (템플릿 문제 우회)
+    @app.route('/')
+    def simple_home():
+        return '''
+        <h1>Big5 성격 테스트</h1>
+        <p>임시 홈페이지 - 템플릿 렌더링을 우회한 버전입니다.</p>
+        <p><a href="/debug">디버그 정보 확인</a></p>
+        <p>원래 앱이 성공적으로 로드되었습니다!</p>
+        '''
         
 except Exception as e:
     # import 에러가 발생하면 간단한 Flask 앱으로 대체
